5269edf9651952495d1324a802dee1abaa8db6c5
Vinimiquelin <vinimiquelin@gmail.com>
2022-06-17 01:28:52 -0300

    Adicionando assets para o projeto e criando script PlayerController

---
+++ Challenge_01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+ using UnityEngine;
+ using System.Collections;
+     
+     public class PlayerController : MonoBehaviour {
+
+         [SerializeField] public float jumpVelocity = 5.0f;
+         [SerializeField] public float walkVelocity = 3.0f;
+         [SerializeField] private Rigidbody2D rigidbody2d;
+         [SerializeField] private SpriteRenderer spriteRenderer;
+         
+         void Start() {
+            rigidbody2d.freezeRotation = true;   
+         }
+         void Update(){
+            Jump();
+            Move();   
+         }
+
+         void Jump(){
+             if(Input.GetKeyDown(KeyCode.Space)){
+     
+                rigidbody2d.velocity = Vector2.up * jumpVelocity;
+             }
+         }
+
+         void Move(){
+             if(Input.GetKeyDown(KeyCode.LeftArrow)){
+                if(spriteRenderer != null)
+                    spriteRenderer.flipX = true;
+                rigidbody2d.velocity = Vector2.left * walkVelocity;
+             }
+
+            if(Input.GetKeyDown(KeyCode.RightArrow)){
+                if(spriteRenderer != null)
+                    spriteRenderer.flipX = false;
+                rigidbody2d.velocity = Vector2.right * walkVelocity;
+             }
+         }
+     }
