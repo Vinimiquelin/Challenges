b022789ae5fc36dd3f1719a14ec7c47b40257919
Vinimiquelin <33907685+Vinimiquelin@users.noreply.github.com>
2022-06-30 11:36:23 -0300

    Adicionando método Fly() no PlayerController.cs

--- Challenge_01/Assets/Scripts/PlayerController.cs
+++ Challenge_01/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,9 @@
  using UnityEngine;
- using System.Collections;
-     
-     public class PlayerController : MonoBehaviour {
 
+ public class PlayerController : MonoBehaviour {
          [SerializeField] public float jumpVelocity = 5.0f;
-         [SerializeField] public float walkVelocity = 3.0f;
+         [SerializeField] public float flyVelocity = 12.0f;
+         [SerializeField] public float moveVelocity = 3.0f;
          [SerializeField] private Rigidbody2D rigidbody2d;
          [SerializeField] private SpriteRenderer spriteRenderer;
          
@@ -13,7 +12,8 @@
          }
          void Update(){
             Jump();
-            Move();   
+            Fly();
+            Move();
          }
 
          void Jump(){
@@ -23,15 +23,22 @@
              }
          }
 
+         void Fly() {
+             if(Input.GetKeyDown(KeyCode.Z)){
+     
+                 rigidbody2d.velocity = Vector2.up * flyVelocity;
+             }
+         }
+
          void Move(){
              if(Input.GetKeyDown(KeyCode.LeftArrow)){
                 spriteRenderer.flipX = true;
-                rigidbody2d.velocity = Vector2.left * walkVelocity;
+                rigidbody2d.velocity = Vector2.left * moveVelocity;
              }
 
             if(Input.GetKeyDown(KeyCode.RightArrow)){
                 spriteRenderer.flipX = false;
-                rigidbody2d.velocity = Vector2.right * walkVelocity;
+                rigidbody2d.velocity = Vector2.right * moveVelocity;
              }
          }
      }
